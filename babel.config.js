a0a78c2e9694cb020605c25451ed527cf84325cf
stacy mednikova <yourniceshot@gmail.com>
2019-10-29 11:34:07 +0300

    second commit

---
+++ babel.config.js
@@ -0,0 +1,22 @@
+const presets = [
+    [
+    "@babel/env",
+    {
+        targets: {
+            edge: "15+",
+            firefox: "60",
+            chrome: "64",
+            safari: "11.1",
+            yandex: "18",
+        },
+            useBuiltIns: "usage", 
+            corejs: "3.0.0", 
+             "targets": { 
+                    "esmodules": true, 
+                     "ie": "11"
+             }
+        },
+    ],
+];
+
+module.exports = { presets };
