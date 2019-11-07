b04c36a70f312ea2ff3deb026c1b4f645b3d9440
stacy mednikova <yourniceshot@gmail.com>
2019-11-07 19:06:29 +0300

    twentieth commit

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,6 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[chunkhash].js',
-        publicPath: '../'
     },
     module: {
         rules: [{
