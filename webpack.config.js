f4ffaf082dc585797aa614586726a522a0a7edee
stacy mednikova <yourniceshot@gmail.com>
2019-11-07 15:34:06 +0300

    fourteenth commit

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[chunkhash].js',
-        publicPath: '../'
+        publicPath: '/dist/'
     },
     module: {
         rules: [{
