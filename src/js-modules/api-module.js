b2c92f48910bd070791e946729399d6e29004af7
stacy mednikova <yourniceshot@gmail.com>
2019-11-21 14:18:06 +0300

    fetch tryouts

--- src/js-modules/api-module.js
+++ src/js-modules/api-module.js
@@ -1,3 +1,9 @@
-class Api {
-    
-}
+fetch('https://newsapi.org/v2/everything?', {
+    headers: {
+        'X-Api-Key': '4d914ea2fc164949aef0bab90a4c40e5',
+    }
+})
+.then(res => res.json())
+.then((res) => {
+    console.log(res)
+})
